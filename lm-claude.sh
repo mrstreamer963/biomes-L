d98d275eacc3f6f21f8a57c3e4cac017099bbd81
mrstreamer963 <698lo7epy@mozmail.com>
2026-07-12 08:13:40 +0300

    add some local run script

---
+++ lm-claude.sh
@@ -0,0 +1,5 @@
+export ANTHROPIC_BASE_URL="http://localhost:1234"
+export ANTHROPIC_API_KEY="dummy"
+export ANTHROPIC_MODEL="de41de6a12c3df44907e3fb9d7059d8187b2ca0d"
+
+claude --bare --exclude-dynamic-system-prompt-sections
---
+++ omlx-claude.sh
@@ -0,0 +1,5 @@
+export ANTHROPIC_BASE_URL="http://127.0.0.1:8000"
+export ANTHROPIC_API_KEY="dummy"
+export ANTHROPIC_MODEL="mlx-community--Qwopus3.6-35B-A3B-Coder-8bit"
+
+claude --bare --exclude-dynamic-system-prompt-sections
